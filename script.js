4f8ae3c29eabbe10662e4e336c35ec5907188d6a
ha0812051-sys <ha0812051@gmail.com>
2026-09-06 21:40:39 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -478,3 +478,21 @@ function escapeHTML(text) {
 ========================= */
 
 showHistory();
+// =========================
+// CONTACT / INFO SECTIONS
+// =========================
+
+// Smooth scrolling for menu links
+document.querySelectorAll('nav a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function (e) {
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            e.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
